cd143202e25482ba8eed1bd65a008ffc3a9464d5
ez8664 <ez8664@g.rit.edu>
2025-09-04 00:11:48 -0400

    upload page done, but didnt check if its working

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -30,9 +30,6 @@ const FileUploader = ({ onFileSelect }: FileUploaderProps) => {
                 <input {...getInputProps()} />
 
                 <div className="space-y-4 cursor-pointer">
-                    <div className="mx-auto w-16 h-16 flex items-center justify-center">
-                        <img src="/icons/info.svg" alt="upload" className="size-20" />
-                    </div>
 
                     {file ? (
                         <div className="uploader-selected-file" onClick={(e) => e.stopPropagation()}>
@@ -47,9 +44,17 @@ const FileUploader = ({ onFileSelect }: FileUploaderProps) => {
                                     </p>
                                 </div>
                             </div>
+                            <button className="p-2 cursor-pointer" onClick={(e) => {
+                                onFileSelect?.(null)
+                            }}>
+                               <img src="/icons/cross.svg" alt="remove" className="w-4 h-4" /> 
+                            </button>
                         </div>
                     ) : (
                         <div>
+                            <div className="mx-auto w-16 h-16 flex items-center justify-center mb-2">
+                                <img src="/icons/info.svg" alt="upload" className="size-20" />
+                            </div>
                             <p className="text-lg text-gray-500">
                                 <span className="font-semibold">
                                     Click to upload
