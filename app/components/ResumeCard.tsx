562019d92676b6bb326f24529ff621038834da97
ez8664 <ez8664@g.rit.edu>
2025-09-09 17:36:40 -0400

    everything in the ResumeCard works fine now

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -5,7 +5,7 @@ import { usePuterStore } from "~/lib/puter";
 
 const ResumeCard = ({ resume: {id, companyName, jobTitle, feedback, imagePath} }: {resume: Resume}) => {
     const { auth, fs } = usePuterStore();
-    const { resumeUrl, setResumeUrl } =  useState('');
+    const [ resumeUrl, setResumeUrl ] =  useState('');
 
     useEffect( () => {
         const loadResume = async() => {
